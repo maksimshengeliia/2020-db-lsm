bfa473287bc96c768795f6118915878f67852716
Maksim Shengeliia <maksim.shengeliia@mail.ru>
2020-04-21 21:24:40 +0300

    implemented DAO interface

---
+++ src/main/java/ru/mail/polis/maksimshengeliia/MyDAO.java
@@ -0,0 +1,45 @@
+package ru.mail.polis.maksimshengeliia;
+
+import org.jetbrains.annotations.NotNull;
+import ru.mail.polis.DAO;
+import ru.mail.polis.Record;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.Iterator;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public class MyDAO implements DAO {
+    private final SortedMap<ByteBuffer, ByteBuffer> map;
+
+    public MyDAO() {
+        map = new TreeMap<>();
+    }
+
+    @NotNull
+    @Override
+    public Iterator<Record> iterator(@NotNull final ByteBuffer from) throws IOException {
+        return map
+                .tailMap(from)
+                .entrySet()
+                .stream()
+                .map(o -> Record.of(o.getKey(), o.getValue()))
+                .iterator();
+    }
+
+    @Override
+    public void upsert(@NotNull final ByteBuffer key, @NotNull final ByteBuffer value) throws IOException {
+        map.put(key, value);
+    }
+
+    @Override
+    public void remove(@NotNull final ByteBuffer key) throws IOException {
+        map.remove(key);
+    }
+
+    @Override
+    public void close() throws IOException {
+        map.clear();
+    }
+}
